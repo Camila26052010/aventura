4133be446a0a6fe5f71784c06b4b5ff84b18cfba
Camila26052010 <ferreira.camila26@escola.pr.gov.br>
2025-09-05 07:44:41 -0300

    jogo

---
+++ script.js
@@ -0,0 +1,7 @@
+function conectarIlhas() {
+    let ilha1 = document.getElementById('ilha1');
+    let ilha2 = document.getElementById('ilha2');
+    ilha1.style.float = 'none';
+    ilha2.style.float = 'none';
+    alert("As ilhas se uniram! O caminho está livre.");
+}
